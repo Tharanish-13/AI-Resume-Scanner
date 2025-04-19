2e9fd5914e1defafc36009b9ae040a85afea433f
THARANISH J <157452382+Tharanish-13@users.noreply.github.com>
2025-04-19 22:27:43 +0530

    Update signin.js

--- frontend/src/pages/signin.js
+++ frontend/src/pages/signin.js
@@ -14,7 +14,7 @@ export default function SignIn() {
     setError("");
 
     try {
-      const response = await axios.post('http://localhost:8000/login', {
+      const response = await axios.post('https://ai-resume-scanner-1e01.onrender.com/login', {
         email,
         password,
       });      
