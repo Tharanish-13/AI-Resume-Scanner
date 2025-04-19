c52e401204d9962828bc03fa295593fc77178367
THARANISH J <157452382+Tharanish-13@users.noreply.github.com>
2025-04-19 17:43:35 +0530

    Update index.js

--- frontend/src/lib/parse-resume-from-pdf/index.js
+++ frontend/src/lib/parse-resume-from-pdf/index.js
@@ -2,3 +2,13 @@ export { readPdf } from "./read-pdf";
 export { groupTextItemsIntoLines } from "./group-text-items-into-lines";
 export { groupLinesIntoSections } from "./group-lines-into-sections";
 export { extractResumeFromSections } from "./extract-resume-from-sections";
+export async function parseResumeFromPdf(fileUrl) {
+  // Dummy parser for now
+  return {
+    workExperiences: [],
+    educations: [],
+    projects: [],
+    skills: { descriptions: [] },
+    custom: { descriptions: [] },
+  };
+}
