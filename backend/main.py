5b6dabf5c7522cde668a4c8b432b204f2061ba7c
THARANISH J <157452382+Tharanish-13@users.noreply.github.com>
2025-04-19 23:00:10 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -21,7 +21,7 @@ app.include_router(resume_router, prefix="/resume", tags=["Resume Routes"])
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://ai-resume-scanner-gamma.vercel.app/"],
+    allow_origins=["https://ai-resume-scanner-gamma.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
